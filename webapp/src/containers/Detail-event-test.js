d65edd63c775bd33756f22528d195d33e472da5a
Tien DTN <tiendtn@asiantech.vn>
2016-12-20 14:58:04 +0700

    style for Event detail cover component

---
+++ webapp/src/containers/Detail-event-test.js
@@ -0,0 +1,10 @@
+import React, {Component, PropTypes}from 'react';
+
+
+class DetailEventtest extends {
+  constructor(props) {
+    super(props);
+    this.statev=b{
+      activeIndex = 0
+    }
+}
