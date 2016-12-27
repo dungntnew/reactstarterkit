a96fd8a50784a6e2f0ac7771e6487cc02f5a72a6
Tien DTN <tiendtn@asiantech.vn>
2016-12-27 17:29:32 +0700

    create blog-detail components

--- webapp/src/containers/BlogDetail.js
+++ webapp/src/containers/BlogDetail.js
@@ -1,6 +1,10 @@
 import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
 
 import '../css/BlogDetail.css';
+
+
+
 
 
 class BlogDetail extends Component {
@@ -10,14 +14,27 @@ class BlogDetail extends Component {
 
   static PropTypes = {
     title: PropTypes.string.isRequired,
+    updated: PropTypes.string.isRequired,
 
   }
+
 
   renderHeader() {
     <div className='blog-heder'>
       <h3 className="ui header">{title}</h3>
+      <div >
+      </div>
 
     </div>
   }
 
+  render() {
+    return(
+      <div className='blog-detail'>
+      </div>
+    )
+  }
+
 }
+
+export default BlogDetail
